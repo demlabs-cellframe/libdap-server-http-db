f3f0b0ed919d37368e86210940ebb9de686879a1
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2019-10-29 18:05:59 +0700

    [*] Some small fixes

--- include/db_core.h
+++ include/db_core.h
@@ -1,17 +1,13 @@
-#ifndef _DATABASE_H_
-#define _DATABASE_H_
-
+#pragma once
 #include <stddef.h>
 #include <mongoc.h>
 
-extern int db_core_init(const char *db_path);
-extern void db_core_deinit();
+int db_core_init(const char *db_path);
+void db_core_deinit();
 
-extern void db_core_refresh();
+void db_core_refresh();
 
-extern int db_input_validation(const char * str);
+int db_input_validation(const char * str);
 
-extern mongoc_client_t *mongo_client, *traffick_track_db_client;
+mongoc_client_t *mongo_client, *traffick_track_db_client;
 
-
-#endif
